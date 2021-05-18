c77152b91496784d79e04933142fe8d8202d3032
Zukhruf <zukhruf.m.f@gmail.com>
2021-05-18 22:20:44 +0700

    Update 10.20PM

--- CI/application/controllers/AdminController.php
+++ CI/application/controllers/AdminController.php
@@ -51,18 +51,15 @@ class AdminController extends CI_Controller
     );
 
     if ($role_karyawan == 'Admin') {
-      $dataCreateUser;
+      $this->adminModel->createUser1($dataCreateUser);
     } else if ($role_karyawan == 'Karyawan'){
-      $dataCreateUser;
-      $dataCreateKaryawan;
+      $this->adminModel->createUser1($dataCreateUser);
+      $this->adminModel->createUser2($dataCreateKaryawan);
     } else if ($role_karyawan == 'Finance'){
-      $dataCreateUser;
-      $dataCreateFinance;
+      $this->adminModel->createUser1($dataCreateUser);
+      $this->adminModel->createUser3($dataCreateFinance);
     }
-
-    $this->adminModel->createUser1($dataCreateUser);
-    $this->adminModel->createUser2($dataCreateKaryawan);
-    $this->adminModel->createUser3($dataCreateFinance);
+    redirect('AdminController');
   }
 
   public function hapusUser($id_user)
